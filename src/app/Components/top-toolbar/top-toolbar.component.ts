76bdb6b7a446c1f4c579a3e82d99942fb300ea00
Ron <ronmada@s.braude.com>
2020-08-02 01:13:05 +0300

    comm

--- src/app/Components/top-toolbar/top-toolbar.component.ts
+++ src/app/Components/top-toolbar/top-toolbar.component.ts
@@ -8,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class TopToolbarComponent implements OnInit {
   toolbarlist = ["צימרים", "מבצעים", "חוות דעת", "אודותינו", "בתי מלון", "וילות", "אטרקציות"]
   constructor() { }
-
+  todom = [
+    { id: "att", name: 'אטרקציות' , options : ["1","two" , "3"] },
+    { id: "miv", name: 'מבצעים' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
   ngOnInit(): void {
   }
 
